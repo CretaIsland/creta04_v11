e3d425d2c7bce56a400bd3857bec5a1c3d4c842c
skpark33 <skpark33333@gmail.com>
2024-10-30 20:35:57 +0900

    0.7.01

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1922198567"
+    serviceWorkerVersion: "2665511942"
   }
 });
